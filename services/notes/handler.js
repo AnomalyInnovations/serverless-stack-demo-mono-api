587427d43b59b605951fe8175d0d0ce8e86dafd0
Frank <frank@seed.run>
2018-08-03 20:48:27 -0400

    Initial commit

---
+++ services/notes/handler.js
@@ -0,0 +1,14 @@
+'use strict';
+  
+module.exports.main = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
