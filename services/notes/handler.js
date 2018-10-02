590f79fbc0c69cad8827011da3b16fd01cd2eaa2
Jay <jvelayud@gmail.com>
2018-10-02 15:59:37 -0400

    Dummy commit to deploy

--- services/notes/handler.js
+++ services/notes/handler.js
@@ -4,7 +4,7 @@ module.exports.main = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Your function executed successfully! ',
       input: event,
     }),
   };
